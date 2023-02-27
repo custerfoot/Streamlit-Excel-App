ba390bfed7a9c5da4c2f60a266436524c2d0453c
custerfoot <79343273+custerfoot@users.noreply.github.com>
2023-02-27 07:52:10 -0600

    Update example_app.py

--- Streamlit Apps/Example Streamlit App/Streamlit Apps/Example Streamlit App/example_app.py
+++ Streamlit Apps/Example Streamlit App/Streamlit Apps/Example Streamlit App/example_app.py
@@ -1,8 +1,8 @@
 
 # import the stuff
 import streamlit as st
-import pandas as pd
-import plotly.express as px
+#import pandas as pd
+#import plotly.express as px
 
 st.set_page_config(page_title = "My App")
 
